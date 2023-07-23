14b533840568d1620e02c98819e9fc479eb7daf3
Smonnnn <slooij1@gmail.com>
2023-07-23 18:16:01 +0200

    Added more methods, changed functions to accept a pointer to an output matrix instead of returning a pointer to a new matrix.

--- matrix.c
+++ matrix.c
@@ -15,13 +15,13 @@ void mat_mult_constant(Matrix* mat, float c){
 	}
 }
 
-Matrix* mat_mult_matrix(Matrix* mat1, Matrix* mat2){
-	if(mat1->width == mat2->heigth){
+void mat_mult_matrix(Matrix* mat1, Matrix* mat2, Matrix* output){
+	if(mat1->width != mat2->heigth){
 		printf("The first matrix's width does not match the second matrix's height!\nCannot compute the product of the two matrices, exiting...\n");
 		exit(EXIT_FAILURE);
 	}
 
-	Matrix output = mat_new(mat2->width, mat1->height);
+	output = mat_new(mat2->width, mat1->height);
 	for(int i = 0; i < output.width; i++){
 		for(int j = 0; j < output.height; j++){
 			for(int k = 0; k < mat1->width; k++){
@@ -29,6 +29,34 @@ Matrix* mat_mult_matrix(Matrix* mat1, Matrix* mat2){
 			}
 		}
 	}
-	return &output;
 }
 
+void mat_add_constant(Matrix* mat, float c){
+	for(int i = 0; i < mat->size; i++){
+		mat->data[i] += c;
+	}
+}
+
+void mat_add_matrix(Matrix* mat1, Matrix* mat2, Matrix* output){
+	if((mat1->height != mat2->heigth) || (mat1->width != mat2->width)){
+		printf("The sizes of the matrices do not match!\nCannot compute the sum of the two matrices, exiting...\n");
+		exit(EXIT_FAILURE);
+	}
+
+	output = mat_new(mat1->width, mat1->height);
+	for(int i = 0; i < mat1->size; i++){
+		output[i] = mat1->data[i] + mat2->data[i];
+	}
+}
+
+void mat_hadamard_product(Matrix* mat1, Matrix* mat2, Matrix* output){
+	if((mat1->height != mat2->heigth) || (mat1->width != mat2->width)){
+		printf("The sizes of the matrices do not match!\nCannot compute the hadamard product of the two matrices, exiting...\n");
+		exit(EXIT_FAILURE);
+	}
+
+	output = mat_new(mat1->width, mat1->height);
+	for(int i = 0; i < mat1->size; i++){
+		output[i] = mat1->data[i] * mat2->data[i];
+	}
+}
--- matrix.h
+++ matrix.h
@@ -1,5 +1,3 @@
-//Functions only return a new matrix when the size changes.
-
 typedef struct Matrix{
 	float* data;
 	int size;
@@ -11,9 +9,11 @@ Matrix* mat_new(int width, int height);
 
 void mat_mult_constant(Matrix* mat, float c);
 
-Matrix* mat_mult_matrix(Matrix* mat1, Matrix* mat2);
+void mat_mult_matrix(Matrix* mat1, Matrix* mat2, Matrix* output);
 
 void mat_add_constant(Matrix* mat, float c);
 
-Matrix* mat_hadamard_product(Matrix* mat1, Matrix* mat2);
+void mat_add_matrix(Matrix* mat1, Matrix* mat2, Matrix* output);
+
+void mat_hadamard_product(Matrix* mat1, Matrix* mat2, Matrix* output);
 
