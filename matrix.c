6645918e7b5458a50715299d6e737f80846bc5cd
Smonnnn <slooij1@gmail.com>
2023-07-25 14:50:10 +0200

    Improved matrix multiplication test and fixed matrix multiplication function.

--- matrix.c
+++ matrix.c
@@ -63,7 +63,7 @@ void mat_mult_matrix(Matrix* mat1, Matrix* mat2, Matrix* output){
 	for(int i = 0; i < output->width; i++){
 		for(int j = 0; j < output->height; j++){
 			for(int k = 0; k < mat1->width; k++){
-				output->data[i + (j * output->width)] += (mat1->data[k + (j * mat1->width)] * mat2->data[k + (i * mat1->width)]);
+				output->data[i + (j * output->width)] += (mat1->data[k + (j * mat1->width)] * mat2->data[i + (k * mat2->width)]);
 			}
 		}
 	}
--- test.c
+++ test.c
@@ -2,14 +2,24 @@
 #include <stdio.h>
 
 int main(){
-	Matrix mat1 = mat_identity(3, 3);
-	Matrix mat2 = mat_identity(3, 3);
+	Matrix mat1 = mat_new(3, 2);
+	Matrix mat2 = mat_new(2, 3);
 	Matrix result_mat_mult;
 	float result_dot;
 
-	mat_set(&mat2, 0, 0, 5.0f);
-	mat_set(&mat2, 1, 1, 4.0f);
-	mat_set(&mat2, 2, 2, 3.0f);
+	mat_set(&mat1, 0, 0, 1.0f);
+	mat_set(&mat1, 1, 0, 2.0f);
+	mat_set(&mat1, 2, 0, 3.0f);
+	mat_set(&mat1, 0, 1, 4.0f);
+	mat_set(&mat1, 1, 1, 5.0f);
+	mat_set(&mat1, 2, 1, 6.0f);
+
+	mat_set(&mat2, 0, 0, 10.0f);
+	mat_set(&mat2, 1, 0, 11.0f);
+	mat_set(&mat2, 0, 1, 20.0f);
+	mat_set(&mat2, 1, 1, 21.0f);
+	mat_set(&mat2, 0, 2, 30.0f);
+	mat_set(&mat2, 1, 2, 31.0f);
 
 	mat_mult_matrix(&mat1, &mat2, &result_mat_mult);
 	mat_dot(&mat1, &mat2, &result_dot);
