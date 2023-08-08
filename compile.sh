8dc5cd6ec35a141a8d3261d05e95021517aafa3f
smonnnn <slooij1@gmail.com>
2023-08-08 21:15:19 +0200

    Fix whitespace in variables.

--- compile.sh
+++ compile.sh
@@ -6,20 +6,20 @@ files="matrix.c"
 
 declare -A dependencies
 
-if ! [ -d $output_location/libs ]; then
-	mkdir $output_location/libs
+if ! [ -d "$output_location"/libs ]; then
+	mkdir "$output_location"/libs
 fi
 
 for d in ${!dependencies[@]}; do
-	if [ -d $output_location/libs/$d/.git ]; then
+	if [ -d "$output_location"/libs/"$d"/.git ]; then
 		git pull origin main
 	else
-		git clone ${dependencies[$d]} $output_location/libs/$d
+		git clone ${dependencies[$d]} "$output_location"/libs/"$d"
 	fi
-	bash $output_location/libs/$d/compile.sh $output_location
-	cp -r $output_location/libs/**/*.h $output_location/libs/
+	bash "$output_location"/libs/"$d"/compile.sh "$output_location"
+	cp -r "$output_location"/libs/**/*.h "$output_location"/libs/
 done
 
-gcc $flags -c $files -o $output_location/libs/$name.o
-ar rcs $output_location/libs/$name.a $output_location/libs/$name.o
-rm $output_location/libs/*.o
+gcc $flags -c "$files" -o "$output_location"/libs/"$name".o
+ar rcs "$output_location"/libs/"$name".a "$output_location"/libs/"$name".o
+rm "$output_location"/libs/*.o
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 name="matrix"
 bash ./compile.sh ./
-gcc test.c -o executable -L./libs/ -l:$name.a
+gcc test.c -o executable -L./libs/ -l:"$name".a
 ./executable
 rm executable
